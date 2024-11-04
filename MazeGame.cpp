201cfbb0e868a9d49ff02f67fb056afa6710a4d7
DestinyN1 <d.newman7@universityofgalway.ie>
2024-11-04 11:57:27 +0000

    added some stuff

--- MazeGame.cpp
+++ MazeGame.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
 #include <string.h>
+#include <vector>
 using namespace std;
+
+int main(){
+    int num_rows;
+    int num_columns;
+    vector<string>> board;
+
+
+    cout << "Enter number of rows:" << endl;
+    cin >> num_rows;
+    cout << "Enter number of columns" << endl;
+    cin >> num_columns;
+
+    for (int i = 0; i < num_rows; i++){
+        vector<string> row;
+     for (int j = 0; j < num_columns; j++){
+
+     
+        if (j == 0)
+         row.push_back("|_|");
+        else
+        row.push_back ("_|");
+    }
+    board.push_back(row);
+    }
+
+    for (int i = 0; i < num_rows; i++){
+        for (int j = 0; j < num_columns; j++){
+            cout << board[i][j].c_str();
+        }
+        cout << board[i][j].c_str();
+    }
+    cout << "\n";
+return 0;
+   }
